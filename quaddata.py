a593a973ec240932d00223b8d8cd47d06190b6a3
ehemdal <erik.hemdal@gmail.com>
2025-09-09 19:51:33 -0400

    Added a graph display for training and test data.  Did some tuning of the quadmodel.
    Changes to be committed:
    	modified:   quaddata.py
    	modified:   quadmodel.py
    	new file:   quadratic_test_data.npz
    	new file:   quadratic_train_data.npz

--- quaddata.py
+++ quaddata.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plot
 
 # Coefficients for y = ax^2 + bx + c
 a = 3
@@ -7,17 +8,36 @@ b = 2
 c = 1
 
 # Generate evenly spaced x values from -10 to 10 into a NumPy array
-x = np.linspace(-10, 10, 1000)
+x = np.linspace(-10, 10, 100)
 
-# Generate y values using the quadratic equation also into a NumPy array.
+# Generate training y values using the quadratic equation also into a NumPy array.
 y = a * x**2 + b * x + c #+ 0.1*np.random.normal(size=1000)
 print(x)
 print(y)
 # Save the data to a file
-np.savez('quadratic_data.npz', x=x, y=y)
+np.savez('quadratic_train_data.npz', x=x, y=y)
+
+# Generate noisy test data
+y = a * x**2 + b * x + c + 0.2*np.random.normal(size=100)
+print(x)
+print(y)
+# Save the data to a file
+np.savez('quadratic_test_data.npz', x=x, y=y)
+
+# plot the training data
+plot.scatter(x, y)
+plot.grid()
+plot.show()
+plot.savefig('quadratic_train_data.png')
+
+# Plot the test data
+plot.scatter(x, y)  
+plot.grid()
+plot.show()
+plot.savefig('quadratic_test_data.png')
 
 # Load the data back from the file to verify it was saved correctly
-data = np.load('quadratic_data.npz')
+data = np.load('quadratic_train_data.npz')
 print(data['x'])
 print(data['y'])
 
--- quadmodel.py
+++ quadmodel.py
@@ -3,25 +3,41 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plot
+
 
 # Load the data back from the file to verify it was saved correctly
-data = np.load('quadratic_data.npz')
+data = np.load('quadratic_train_data.npz')
 xs = data['x']
 ys = data['y']  
 
 # Define the model
 model = Sequential([
     Dense(units=1,activation='linear',input_shape=[1]),
-    Dense(units=64, activation='relu'),
-    Dense(units=64, activation='relu'),
+    Dense(units=80, activation='relu'),
+    Dense(units=80, activation='relu'),
     Dense(units=1,activation='linear')
 ])      
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
 model.summary()
 # Train the model
-model.fit(xs, ys, epochs=1000, verbose=0) 
-# Make a prediction for x = 10.0
-print(model.predict(np.array([10.0])))  
+model.fit(xs, ys, epochs=250, verbose=0) 
+# Make a prediction for x = 5.0 should be 86
+print(model.predict(np.array([5.0])))  
+
+# Try the model using test data
+testdata = np.load('quadratic_test_data.npz')
+test_xs = testdata['x']
+test_ys = testdata['y']
+test_loss, test_mae = model.evaluate(test_xs, test_ys, verbose=2)
+print('\nTest MAE:', test_mae)
+test_ys = model.predict(test_xs)
+# Plot the test data
+plot.scatter(test_xs, test_ys)
+plot.grid()
+plot.show()
+plot.savefig('quadratic_test_results.png')  
+
 # Display the weights learned by the model
-for index, layer in enumerate(model.layers):
-    print("Layer {}: {}".format(index, model.get_weights()))    
+# for index, layer in enumerate(model.layers):
+#     print("Layer {}: {}".format(index, model.get_weights()))    
