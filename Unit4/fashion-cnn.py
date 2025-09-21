091222b68729bfac8bd93684c0f0a493028761fe
ehemdal <erik.hemdal@gmail.com>
2025-09-21 15:03:06 -0400

    Final tweaks to fashion-cnn with my annotations.

--- Unit4/fashion-cnn.py
+++ Unit4/fashion-cnn.py
@@ -13,43 +13,51 @@
 import tensorflow as tf
 data = tf.keras.datasets.fashion_mnist
 
+# Define a callback to stop training once we reach 99% accuracy.
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('accuracy')>0.99):
       print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
 
-
+# Register the callback.
 callbacks = myCallback()
 
-
 (training_images, training_labels), (test_images, test_labels) = data.load_data()
-print(type(training_images))
+
+print("The training images are of type " + str(type(training_images)))
+
+# Reshape the images to 2D and normalize the pixel values.
 training_images=training_images.reshape(60000, 28, 28, 1)
 training_images  = training_images / 255.0
 test_images = test_images.reshape(10000, 28, 28, 1)
 test_images = test_images / 255.0
 
+# Here's the CNN model.  Rather than using the add() method to add layers one at a time,
+# this code passes a list of layers to the Sequential() method.
 model = tf.keras.models.Sequential([
-    #tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.InputLayer(shape=(28, 28, 1)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Flatten(),
+# Notice that we flatten the images only after all the convolution and pooling layers.
+# The rest of this model is the same as in the previous example.    
+    tf.keras.layers.Flatten(), 
     tf.keras.layers.Dense(128, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
 
+# We are guessing that 50 epochs should be enough to get to 99% accuracy.
+# The callback will interrupt training if we reach that goal sooner.
 model.fit(training_images, training_labels, epochs=50, callbacks=[callbacks])
 
 model.evaluate(test_images, test_labels)
 
+# Feel free to change the index here to see the result for a different image.
 classifications = model.predict(test_images)
 print(classifications[500])
 print(test_labels[500])
