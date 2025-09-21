4547e8687f663b4b0264dfb62e15c095d4bf8ae7
ehemdal <erik.hemdal@gmail.com>
2025-09-21 13:59:40 -0400

    Updates to unit 4

--- Unit4/fashion_callback.py
+++ Unit4/fashion_callback.py
@@ -14,6 +14,7 @@ mnist = tf.keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 training_images=training_images/255.0
 test_images=test_images/255.0
+
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation=tf.nn.relu),
