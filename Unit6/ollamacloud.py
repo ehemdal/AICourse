5b646759598cff0a69c8a9ae2836998afb523fcf
ehemdal <erik.hemdal@gmail.com>
2025-10-19 15:49:13 -0400

    Add a cloud example for unit 6

---
+++ Unit6/ollamacloud.py
@@ -0,0 +1,16 @@
+import os
+from ollama import Client
+
+client = Client(
+    host="https://ollama.com",
+    headers={'Authorization': 'Bearer ' + str(os.environ.get('OLLAMA_API_KEY'))}
+)
+messages = [
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+]
+
+for part in client.chat('gpt-oss:120b', messages=messages, stream=True):
+  print(part['message']['content'], end='', flush=True)
